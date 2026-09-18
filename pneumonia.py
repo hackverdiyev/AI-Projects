6ebc5ebd1b7427611865516a4ea2003e9a2f8e66
hackverdiyev <haqverdiyevturab@gmail.com>
2026-09-18 19:53:23 +0900

    Data Change

--- pneumonia.py
+++ pneumonia.py
@@ -8,16 +8,20 @@ from torch.utils.data import DataLoader
 train_dir="./Data/Pneumonia/train"
 test_dir="./Data/Pneumonia/test"
 
+torch.manual_seed(42)
+
 transform_train=transforms.Compose([ 
     transforms.Resize((150, 150)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(20),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.RandomEqualize(p=1.0),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
 ])
 
 transform_test=transforms.Compose([
     transforms.Resize((150, 150)),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.RandomEqualize(p=1.0),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
 ])
@@ -35,25 +39,34 @@ class PneumoniaCNN(nn.Module):
     def __init__(self):
         super(PneumoniaCNN,self).__init__()
         self.conv_layers=nn.Sequential(
-            nn.Conv2d(3,32,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(1,32,kernel_size=3,stride=1,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
             
             nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
             
             nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,stride=2)
+            nn.MaxPool2d(kernel_size=2,stride=2),
+
+            nn.Dropout2d(0.3),
+            nn.Conv2d(128,256,kernel_size=3,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.AdaptiveAvgPool2d((4,4))
         )
         self.fc_layers=nn.Sequential(
             nn.Flatten(),
-            nn.Linear(128*18*18,128),
+            nn.Linear(256*4*4,128),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128,1),
-            nn.Sigmoid()
+            nn.Dropout(0.4),
+            nn.Linear(128,1)
         )
     
     def forward(self, x):
@@ -66,10 +79,13 @@ device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model=PneumoniaCNN().to(device)
 
 print("CNN created")
+print("PyTorch CUDA version:", torch.version.cuda)
+if torch.cuda.is_available():
+    print("Selected device:", torch.cuda.get_device_name(0))
 
 
-criterion=nn.BCELoss()  
-optimizer=optim.Adam(model.parameters(), lr=0.001)
+criterion=nn.BCEWithLogitsLoss(pos_weight=torch.tensor([1341/3875],device=device))  
+optimizer=optim.Adam(model.parameters(),lr=0.0005,weight_decay=0.0001)
 
 def train_model(model,train_loader,test_loader,criterion,optimizer,epochs):
     train_loss,test_loss,test_acc=[],[],[]
@@ -80,11 +96,11 @@ def train_model(model,train_loader,test_loader,criterion,optimizer,epochs):
             images,labels=images.to(device),labels.float().to(device)
             optimizer.zero_grad()
             outputs=model(images)
-            loss=criterion(outputs.squeeze(),labels)
+            loss=criterion(outputs.squeeze(1),labels)
             loss.backward()
             optimizer.step()
-            epoch_loss+=loss.item()
-        train_loss.append(epoch_loss/len(train_loader))
+            epoch_loss+=loss.item()*images.size(0)
+        train_loss.append(epoch_loss/len(train_loader.dataset))
         
         model.eval()
         correct,total,test_epoch_loss=0,0,0
@@ -92,17 +108,17 @@ def train_model(model,train_loader,test_loader,criterion,optimizer,epochs):
             for images,labels in test_loader:
                 images,labels=images.to(device),labels.float().to(device)
                 outputs=model(images)
-                test_loss_batch=criterion(outputs.squeeze(),labels)
-                test_epoch_loss+=test_loss_batch.item()
-                preds=(outputs.squeeze()>0.5).float()
+                test_loss_batch=criterion(outputs.squeeze(1),labels)
+                test_epoch_loss+=test_loss_batch.item()*images.size(0)
+                preds=(outputs.squeeze(1).sigmoid()>0.4).float()
                 correct+=(preds==labels).sum().item()
                 total+=labels.size(0)
-        val_loss.append(val_epoch_loss/len(val_loader))
-        val_acc.append(correct/total)
+        test_loss.append(test_epoch_loss/total)
+        test_acc.append(correct/total)
         print(f"Epoch {epoch+1}/{epochs}, Train Loss: {train_loss[-1]:.4f}, "
-              f"Val Loss: {test_loss[-1]:.4f}, Val Acc: {test_acc[-1]*100:.2f}%")
+              f"Test Loss: {test_loss[-1]:.4f}, Test Acc: {test_acc[-1]*100:.2f}%")
     
     return train_loss,test_loss,test_acc
 
-train_loss,test_loss,test_acc=train_model(model,train_loader,test_loader,criterion,optimizer,epochs=50)
+train_loss,test_loss,test_acc=train_model(model,train_loader,test_loader,criterion,optimizer,epochs=35)
 
